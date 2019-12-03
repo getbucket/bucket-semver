876a3cd774eb41eb5273c0edf7835f576e64cbe6
yumenghan <yumenghan@ztgame.com>
2019-12-03 16:36:06 +0800

    Repository address changed to getbucket/semver

--- src/Bucket.Semver/Properties/AssemblyInfo.cs
+++ src/Bucket.Semver/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * Document: https://github.com/getbucket/bucket-semver/wiki
+ * Document: https://github.com/getbucket/semver/wiki
  */
 
 using System.Runtime.CompilerServices;
